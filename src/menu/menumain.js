debf74d5a1267849e1f00a9b9b94e5b56ba5768c
Odle <bodle@sbma.net>
2022-05-27 16:09:46 -0700

    .

---
+++ src/menu/menumain.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+
+    
+
+    return (
+        <div id="menu">
+            <div id="play" className="menubtn">play</div>
+            <div id="howto" className="menubtn">how to</div>
+            <div id="credits" className="menubtn">credits</div>
+        </div>
+    )
+}
+
+export default Menu;
