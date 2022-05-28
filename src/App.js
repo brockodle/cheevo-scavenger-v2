9edd2e43f69c355b933f9eedf50869e818270478
Brock <brockodle@gmail.com>
2022-05-28 01:03:47 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './menu/menumain';
+import { Router } from 'react-router-dom';
 
 function App() {
+
   return (
     <Menu/>
   );
--- src/menu/menumain.js
+++ src/menu/menumain.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./menu.css";
 
 const Menu = () => {
 
-    
+    const [c, topage] = useState();
+
+
 
     return (
         <div>
-        <div id="headertxt">
-            Cheevo Scavenger
-        </div>
-        <div id="menu">
-            <div id="play" className="menubtn">play</div>
-            <div id="howto" className="menubtn">how to</div>
-            <div id="credits" className="menubtn">credits</div>
-        </div>
+            <div id="headertxt">
+                Cheevo Scavenger
+            </div>
+            <div id="menu">
+                <div id="play" onClick={() => topage()} className="menubtn">play</div>
+                <div id="howto" className="menubtn">how to</div>
+                <div id="credits" className="menubtn">credits</div>
+            </div>
         </div>
     )
 }
