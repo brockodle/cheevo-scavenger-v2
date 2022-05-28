7d2ebe080aabb3e65b7bd44e2b7a1eb33d97a1e1
Brock <brockodle@gmail.com>
2022-05-27 23:36:04 -0700

    .

--- src/menu/menumain.js
+++ src/menu/menumain.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "./menu.css";
 
 const Menu = () => {
 
     
 
     return (
+        <div>
+        <div id="headertxt">
+            Cheevo Scavenger
+        </div>
         <div id="menu">
             <div id="play" className="menubtn">play</div>
             <div id="howto" className="menubtn">how to</div>
             <div id="credits" className="menubtn">credits</div>
+        </div>
         </div>
     )
 }
