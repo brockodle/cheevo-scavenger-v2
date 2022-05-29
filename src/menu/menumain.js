f04c8970eae6af109d6251db523e84288991c539
brockodle <48767869+brockodle@users.noreply.github.com>
2022-05-29 01:28:02 -0700

    .

--- src/menu/menumain.js
+++ src/menu/menumain.js
@@ -3,9 +3,23 @@ import "./menu.css";
 
 const Menu = () => {
 
+    function Promptmain() {
+        return (
+            <div id="clickhappened">
+                A click happened
+            </div>
+        )
+    }
+
+    const logclick = () => {
+        console.log('make stuff happen here.');
+
+        return (
+            <Promptmain/>
+        )
+    };
+
     const [c, topage] = useState();
-
-
 
     return (
         <div>
@@ -13,9 +27,15 @@ const Menu = () => {
                 Cheevo Scavenger
             </div>
             <div id="menu">
-                <div id="play" onClick={() => topage()} className="menubtn">play</div>
-                <div id="howto" className="menubtn">how to</div>
-                <div id="credits" className="menubtn">credits</div>
+                <div id="play" onClick={() => topage(logclick)} className="menubtn">
+                    play
+                </div>
+                <div id="howto" className="menubtn">
+                    how to
+                </div>
+                <div id="credits" className="menubtn">
+                    credits
+                </div>
             </div>
         </div>
     )
