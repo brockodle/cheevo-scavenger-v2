f8760bf327944ab46bbbc9299799cc7ad5bbbd90
Brock <brockodle@gmail.com>
2022-06-05 00:08:08 -0700

    .

--- src/game/maingame.js
+++ src/game/maingame.js
@@ -4,19 +4,44 @@ import cheevos from "../cheevorepo/mall.json"
 import "./game.css";
 import $ from "jquery";
 
-const Timer = () => {
-
-    return (
-        <div id="timer">
-
-        </div>
-    )
-}
-
 const Game = () => {
+
+    const [p, pointsout] = useState(0);
+
+    const incpts = () => {
+        
+        p++
+
+        return (
+            <span>{p}</span>
+        )
+    }
+
+    const Timer = () => {
+
+        return (
+            <div id="timer">
+
+            </div>
+        )
+    }
 
     const mall = [];
     const displaycheevs = [];
+
+    function fourcheevs() {
+        for (let c = 0; c < 4; c++) {
+            const ch = mall[Math.round(Math.random() * mall.length)];
+            for (let i of displaycheevs) {
+                if (ch.title === i.title) {
+                    c--;
+                    console.log('title is same');
+                    fourcheevs();
+                }
+            }
+            displaycheevs.push(ch);
+        }
+    };
 
     (function mallcheevos() {
         const allcheevs = cheevos.achievements;
@@ -24,19 +49,6 @@ const Game = () => {
             const e = allcheevs[d];
             mall.push({ 'title': e.title, 'description': e.description, 'points': e.points, 'image': e.vid });
         }
-        function fourcheevs() {
-            for (let c = 0; c < 4; c++) {
-                const ch = mall[Math.round(Math.random() * mall.length)];
-                for (let i of displaycheevs){
-                    if (ch.title === i.title){
-                        c--;
-                        console.log('title is same');
-                        fourcheevs();
-                    }
-                }
-                displaycheevs.push(ch);     
-            }
-        };
         fourcheevs();
     }());
 
@@ -49,44 +61,64 @@ const Game = () => {
 
         $('#description').hide().fadeIn(200);
 
-        const newcheevo = (newid, pts, id) => {
-            $('.cheevoitem-pointsbtn').on('tap, click', function(){
-                $(`#${id}`).animate({
-                    'left':'110%'
-                }, 200).remove();
-            });
-        }
+        const adddescription = (cheevodesc, pts, cheevoid) => {
 
-        const adddescription = (cheevodesc, pts) => {
-
-            return(
+            return (
                 <div id="description">
-                    <div className="desccopy">{cheevodesc}</div>
-                    <button onClick={replacement(newcheevo())} className="cheevoitem-pointsbtn">Found it! ({pts} points)</button>
+                    <div className="desccopy">
+                        {cheevodesc}
+                    </div>
+                    <button onClick={() => replacement(newcheevo(cheevoid))} className="cheevoitem-pointsbtn">
+                        Found it! ({pts} points)
+                    </button>
                 </div>
             )
         }
 
         const outdivs = displaycheevs.map(
             (item, index) => (
-                <div id={index} className="cheevoitem" onClick={() => makenew(adddescription(item.description, item.points))} key={index}>
+                <div id={index} className="cheevoitem" onClick={() => makenew(adddescription(item.description, item.points, index))} key={index}>
                     {item.title}
-                    {newc}
                 </div>
             )
         );
 
+        const newcheevo = (id) => {
+
+            const olditem = document.getElementById(id);
+            const desc = document.getElementById('description');
+
+            $(olditem)
+                .animate({
+                    "left": "110%",
+                }, 150)
+                .delay(250, function () {
+                    $(olditem).remove();
+                    $(desc).hide();
+                });
+
+            displaycheevs.slice(id);
+            const newitem = mall[Math.round(Math.random() * mall.length)];
+            displaycheevs.push(newitem);
+        }
+
         return (
             <div id="cheevoparent" className="cheevoparent" key="cheevoparent">
                 {outdivs}
+                {newc}
                 {cheevo}
             </div>
         )
     }
 
+    const Points = () => {
+
+    }
+
     return (
         <div>
-            <Timer/>
+            <Timer />
+            <div className="points">{p}</div>
             <OptionDivs />
         </div>
     )
