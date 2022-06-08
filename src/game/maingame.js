fec0f1704131bb2836ef0dd94153dd31921791a0
brockodle <48767869+brockodle@users.noreply.github.com>
2022-06-07 20:30:03 -0700

    .

--- src/game/maingame.js
+++ src/game/maingame.js
@@ -13,29 +13,44 @@ import $ from "jquery";
 const Game = () => {
 
     const [p, addpts] = useState(0);
+    const [c, cheevdetail] = useState();
     let mallcheevos = [];
     let currcheevs = [];
 
+    const expandinfo = (allp, item) => {
+
+        const iid = allp.props.id.toString();
+        const cid = allp.props.className.toString();
+
+        console.log(allp, item);
+
+        $('#' + iid + '.' + cid).toggleClass('cheevselect');
+
+        return (
+            <div>{item.description}</div>
+        )
+
+    }
+
     const set4cheevos = () => {
 
-        for (let cu = 0; cu < 4; cu++) {
+        for (let cu = 0; cu < 5; cu++) {
 
             const pick = mallcheevos[Math.floor(Math.round(Math.random() * mallcheevos.length))];
-            
-            if (currcheevs.length > 1){
 
-                for (let p of currcheevs){
+            if (currcheevs.length > 1) {
 
-                    if (p.title === pick.title){
+                for (let p of currcheevs) {
 
-                        console.log('same title', p.title, pick.title, currcheevs.indexOf(p));
-                        currcheevs.splice(currcheevs.indexOf(p),1);
+                    if (p.title === pick.title) {
+
+                        currcheevs.splice(currcheevs.indexOf(p), 1);
                         cu--;
-                        
+
                     }
 
                     else {
-                        console.log('different titles', p.title, pick.title);
+
                     }
                 }
             }
@@ -70,15 +85,26 @@ const Game = () => {
 
     const Cheevos = () => {
 
+        const outdivs = currcheevs.map(
+            (item, index) => (
+                <div id={index} className="cheevoitem" key={index} onClick={() => cheevdetail(expandinfo(outdivs[index], item))} >
+                    {item.title}
+                </div>
+            )
+        );
+
         return (
-            <div className="cheevoitem">Item X</div>
+            outdivs
         )
+
     }
 
     const CheevoDescription = () => {
 
         return (
-            <div id="description" >Description of item</div>
+            <div id="description">
+                {c}
+            </div>
         )
     }
 
@@ -92,7 +118,7 @@ const Game = () => {
     return (
         <div className="cheevoparent">
             <Points id="points" /><Timer id="timer" />
-            <Cheevos />
+            <Cheevos dtl={cheevdetail} />
             <CheevoDescription />
             <PtsBtn add={addpts} />
         </div>
